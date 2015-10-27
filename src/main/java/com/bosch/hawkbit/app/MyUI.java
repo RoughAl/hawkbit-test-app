cfc0e32e3efb566a8ca02323a922261c8ceed6a0
Michael Hirsch <michael.hirsch@bosch-si.com>
2015-10-27 17:36:39 +0100

    Make it run
    Signed-off-by: Michael Hirsch <michael.hirsch@bosch-si.com>

---
+++ src/main/java/com/bosch/hawkbit/app/MyUI.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2011-2015 Bosch Software Innovations GmbH, Germany. All rights reserved.
+ */
+package com.bosch.hawkbit.app;
+
+import com.bosch.sp.server.ui.DefaultHawkbitUI;
+import com.vaadin.spring.annotation.SpringUI;
+
+@SpringUI
+public class MyUI extends DefaultHawkbitUI {
+
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+
+}
