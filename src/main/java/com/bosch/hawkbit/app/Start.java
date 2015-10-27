a3521cc602762e25555af93e899a697179bf20a7
Michael Hirsch <michael.hirsch@bosch-si.com>
2015-10-27 15:39:17 +0100

    initial commit of the test-hawkbit-application
    Signed-off-by: Michael Hirsch <michael.hirsch@bosch-si.com>

---
+++ src/main/java/com/bosch/hawkbit/app/Start.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2011-2015 Bosch Software Innovations GmbH, Germany. All rights reserved.
+ */
+package com.bosch.hawkbit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.bosch.hawkbit.autoconfigure.security.EnableHawkbitManagedSecurityConfiguration;
+import com.bosch.sp.server.RepositoryApplicationConfiguration;
+
+/**
+ * @author imm0344
+ *
+ */
+@SpringBootApplication
+@Import( RepositoryApplicationConfiguration.class )
+@EnableHawkbitManagedSecurityConfiguration
+public class Start {
+
+   /**
+    * Main method to start the spring-boot application.
+    * @param args the VM arguments.
+    */
+   public static void main( final String[] args ) {
+      SpringApplication.run( Start.class, args );
+   }
+}
