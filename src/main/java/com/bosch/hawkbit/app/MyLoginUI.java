97a10b62c850845511581dc80b13c93684127352
SirWayne <dennis.melzer@bosch-si.com>
2015-10-28 10:39:28 +0100

    Add LoginUi
    Signed-off-by: SirWayne <dennis.melzer@bosch-si.com>

--- src/main/java/com/bosch/hawkbit/app/MyLoginUI.java
+++ src/main/java/com/bosch/hawkbit/app/MyLoginUI.java
@@ -3,15 +3,13 @@
  */
 package com.bosch.hawkbit.app;
 
-import com.bosch.sp.server.ui.HawkbitUI;
+import com.bosch.sp.server.ui.login.HawkbitLoginUI;
+import com.bosch.sp.server.ui.themes.HawkbitTheme;
 import com.vaadin.spring.annotation.SpringUI;
 
-@SpringUI
-public class MyUI extends HawkbitUI {
+@SpringUI( path = HawkbitTheme.LOGIN_UI_PATH )
+public class MyLoginUI extends HawkbitLoginUI {
 
-   /**
-    * 
-    */
    private static final long serialVersionUID = 1L;
 
 }
